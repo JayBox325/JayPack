c55d0c943f252efc06a4443bcdc6426a086910cb
Jay Collett <hello@jaycollett.co>
2019-01-28 20:45:03 +0000

    config and stripping out not compulsory components

--- _gulp/tasks/_index.js
+++ _gulp/tasks/_index.js
@@ -1,5 +1,6 @@
 import gulp from 'gulp'
 import requireDir from 'require-dir'
+import notify from 'gulp-notify'
 
 requireDir('./', {recurse: true})
 
@@ -8,58 +9,43 @@ import config from '../config'
 
 // Build then watch, but this doesn't move or optimise assets such as images, videos etc.
 if (config.project == 'static') {
-	gulp.task('default', gulp.series(
-		'clean',
-		gulp.parallel(
-			'svg',
-			'styles',
-			'scripts',
-			'nunjucks',
-		),
+	gulp.task('default', gulp.parallel(
+		'svg',
+		'styles',
+		'scripts',
+		'nunjucks',
 		'serve',
 		'watch'
 	))
 } else {
-	gulp.task('default', gulp.series(
-		'clean',
-		gulp.parallel(
-			'scripts',
-			'styles',
-			'svg'
-		),
+	gulp.task('default', gulp.parallel(
+		'scripts',
+		'styles',
+		'svg',
 		'watch'
 	))
 }
 
 // Build
 if (config.project == 'static') {
-	gulp.task('build', gulp.series(
-		'clean',
-		gulp.parallel(
-			'styles',
-			'scripts',
-			'move-favicons',
-			'move-fonts',
-			'move-videos',
-			'images',
-			'svg',
-			'nunjucks'
-		),
-		'sizereport'
+	gulp.task('build', gulp.parallel(
+		'styles',
+		'scripts',
+		'move-favicons',
+		'move-fonts',
+		'move-videos',
+		'images',
+		'svg',
+		'nunjucks'
 	))
 } else {
-	gulp.task('build', gulp.series(
-		'clean',
-		gulp.parallel(
-			'styles',
-			'scripts',
-			'move-favicons',
-			'move-fonts',
-			'move-videos',
-			'images',
-			'svg',
-			'inject-assets',
-			'sizereport'
-		)
+	gulp.task('build', gulp.parallel(
+		'styles',
+		'scripts',
+		'move-favicons',
+		'move-fonts',
+		'move-videos',
+		'images',
+		'svg'
 	))
 }
