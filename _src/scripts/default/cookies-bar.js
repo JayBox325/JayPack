69ee214b03c5cbe3934e4ffceebdbdcf4fe39e3d
Jaybox325 <hello@jaycollett.co>
2020-11-19 11:00:20 +0000

    cookie bar

---
+++ _src/scripts/default/cookies-bar.js
@@ -0,0 +1,25 @@
+import Cookies from 'js-cookie'
+
+export default function cookieBanner() {
+    const cookieBar = document.querySelector('[data-cookie-bar]')
+    const cookieAccept = document.querySelector('[data-cookie-accept]')
+    const activeClass = 'is-active'
+
+    console.log(banner)
+
+    if (Cookies.get('stp-cookies-accept')) {
+        cookieBar.remove()
+    } else {
+        cookieBar.classList.add(activeClass)
+        cookieAccept.addEventListener('click', acceptCookies.bind(this))
+    }
+
+    function acceptCookies() {
+        Cookies.set('stp-cookies-accept', true, { expires: 730 })
+        cookieBar.classList.remove(activeClass)
+    }
+
+    function resetCookie() {
+        Cookies.remove('stp-cookies-accept')
+    }
+}
