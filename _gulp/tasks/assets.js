2e71baba43213150f87a12a539151f34d205ca5a
Jaybox325 <hello@jaycollett.co>
2021-01-12 15:48:01 +0000

    checking for assets before running gulp task

--- _gulp/tasks/assets.js
+++ _gulp/tasks/assets.js
@@ -13,36 +13,48 @@ const development = config.env.development
 // Assets packages
 import imagemin from 'gulp-imagemin'
 import changed from 'gulp-changed'
+import fs from 'fs'
 
 // Minify and move images
-gulp.task('images', () => {
-    return gulp.src(paths.assets.images.src)
-        .pipe(changed(paths.assets.images.src))
-        .pipe(imagemin())
-        .on('error', handleErrors)
-        .pipe(gulp.dest(paths.assets.images.dest))
-        .on('error', handleErrors)
-        .pipe(development(browserSync.reload({ stream: true })))
-        .on('end', function() {
-            notify({
-                title: '👍 Images compiled & moved',
-                message: 'JayPack Reloaded'
-            }).write('')
-        })
+gulp.task('images', (done) => {
+    if (fs.existsSync(paths.assets.images.src)) {
+        return gulp.src(paths.assets.images.src)
+            .pipe(changed(paths.assets.images.src))
+            .pipe(imagemin())
+            .on('error', handleErrors)
+            .pipe(gulp.dest(paths.assets.images.dest))
+            .on('error', handleErrors)
+            .pipe(development(browserSync.reload({ stream: true })))
+            .on('end', function() {
+                notify({
+                    title: '👍 Images compiled & moved',
+                    message: 'JayPack Reloaded'
+                }).write('')
+            })
+    }
+    done()
 })
 
 // Move videos
-gulp.task('move-videos', () => {
-    return gulp.src(paths.assets.videos.src)
-        .pipe(gulp.dest(paths.assets.videos.dest))
-        .on('error', handleErrors)
-        .pipe(development(browserSync.reload({ stream: true })))
+gulp.task('move-videos', (done) => {
+    if (fs.existsSync(paths.assets.videos.src)) {
+        return gulp.src(paths.assets.videos.src)
+            .pipe(changed(paths.assets.videos.src))
+            .pipe(gulp.dest(paths.assets.videos.dest))
+            .on('error', handleErrors)
+            .pipe(development(browserSync.reload({ stream: true })))
+    }
+    done()
 })
 
 // Move favicons
-gulp.task('move-favicons', () => {
-    return gulp.src(paths.assets.favicons.src)
-        .pipe(gulp.dest(paths.assets.favicons.dest))
-        .on('error', handleErrors)
-        .pipe(development(browserSync.reload({ stream: true })))
+gulp.task('move-favicons', (done) => {
+    if (fs.existsSync(paths.assets.favicons.src)) {
+        return gulp.src(paths.assets.favicons.src)
+            .pipe(changed(paths.assets.favicons.src))
+            .pipe(gulp.dest(paths.assets.favicons.dest))
+            .on('error', handleErrors)
+            .pipe(development(browserSync.reload({ stream: true })))
+    }
+    done()
 })
