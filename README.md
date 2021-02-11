d4f58a403df4b80efc5e3573508ad1bd33eceba4
Jaybox325 <hello@jaycollett.co>
2021-02-11 17:51:47 +0000

    added custom font sizes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JayPack 🍾
 A Frontend boilterplate using Webpack & Gulp 4. Read the docs in the [JayPack Wiki](https://github.com/JayBox325/JayPack/wiki).
+
+### Shopify
+
+`theme open -e development` Open the development theme in the browser.
--- _gulp/tailwind.config.js
+++ _gulp/tailwind.config.js
@@ -32,6 +32,21 @@ module.exports = {
                 xl: '1200px',
                 '2xl': '1600px'
             },
+            fontSize: {
+                xs: '0.75rem',      // 12px
+                sm: '0.875rem',     // 14px
+                base: '1rem',       //  6px
+                lg: '1.125rem',     // 18px
+                xl: '1.25rem',      // 20px
+                '2xl': '1.5rem',    // 24px
+                '3xl': '1.875rem',  // 30px
+                '4xl': '2.25rem',   // 36px
+                '5xl': '3rem',      // 48px
+                '6xl': '3.75rem',   // 60px
+                '7xl': '4.5rem',    // 72px
+                '8xl': '6rem',      // 96px
+                '9xl': '8rem',      // 128px
+            },
             spacing: {
                 px: '1px',
                 '0': '0',
